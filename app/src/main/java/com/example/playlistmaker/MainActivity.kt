9735addd3896f9f1be665026af0160fe68b9c6cb
Kvadrokom <lortrem@mail.ru>
2024-05-26 20:10:28 +0300

    second homework

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         findViewById<Button>(R.id.button_search).setOnClickListener {
             startActivity(
                 Intent(
